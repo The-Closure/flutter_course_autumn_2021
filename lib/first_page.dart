1cfb0258d244d6e673fe945e8055a9e0434ef7cb
anasa <anasa@techno-visions.net>
2021-11-10 21:05:24 +0200

    drawer

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,11 +1,78 @@
 import 'package:flutter/material.dart';
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({Key key}) : super(key: key);
+class FirstPage extends StatefulWidget {
+  FirstPage({Key key}) : super(key: key);
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  String title;
+
+  int _selectedDestination = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Container(
+          width: double.infinity,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text('username'),
+                accountEmail: Text('email'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'Header',
+                ),
+              ),
+              Divider(
+                height: 1,
+                thickness: 1,
+              ),
+              ListTile(
+                leading: Icon(Icons.favorite),
+                title: Text('Item 1'),
+                selected: _selectedDestination == 0,
+                onTap: () => selectDestination(0),
+              ),
+              ListTile(
+                leading: Icon(Icons.delete),
+                title: Text('Item 2'),
+                selected: _selectedDestination == 1,
+                onTap: () => selectDestination(1),
+              ),
+              ListTile(
+                leading: Icon(Icons.label),
+                title: Text('Item 3'),
+                selected: _selectedDestination == 2,
+                onTap: () => selectDestination(2),
+              ),
+              Divider(
+                height: 1,
+                thickness: 1,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'Label',
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.bookmark),
+                title: Text('Item A'),
+                selected: _selectedDestination == 3,
+                onTap: () => selectDestination(3),
+              ),
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: Text('data'),
         leading: Icon(Icons.menu),
@@ -27,5 +94,11 @@ class FirstPage extends StatelessWidget {
         child: Icon(Icons.add),
       ),
     );
+  }
+
+  void selectDestination(int index) {
+    setState(() {
+      _selectedDestination = index;
+    });
   }
 }
