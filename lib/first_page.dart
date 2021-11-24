5548d297cf32cb8e1b6717df34f84c18254da01f
anasa <anasa@techno-visions.net>
2021-11-24 20:25:02 +0200

    init point

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -12,6 +12,7 @@ class _FirstPageState extends State<FirstPage> {
   int _groupRadio = -1;
   bool _switch = false;
   double _slider = 10;
+  String title = 'date value';
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +20,23 @@ class _FirstPageState extends State<FirstPage> {
       initialIndex: 3,
       length: 4,
       child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.date_range),
+          onPressed: () async {
+            final date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1990, 1),
+                lastDate: DateTime(2030, 1));
+            print(date);
+            setState(() {
+              title =
+                  '${date.year.toString()}-${date.month.toString()}-${date.day.toString()}';
+            });
+          },
+        ),
         appBar: AppBar(
-          title: Text('hello tab bar'),
+          title: Text('$title'),
           bottom: TabBar(
             tabs: [
               Tab(
@@ -69,16 +85,27 @@ class _FirstPageState extends State<FirstPage> {
               child: Column(
                 children: [
                   for (var i = 0; i < 5; i++)
-                    RadioListTile(
-                      title: Text('data'),
-                      value: i,
-                      groupValue: _groupRadio,
-                      onChanged: (value) {
-                        setState(() {
-                          _groupRadio = value;
-                        });
-                      },
-                      activeColor: Color(0xFF6200EE),
+                    Column(
+                      children: [
+                        RadioListTile(
+                          title: Text('data'),
+                          value: i,
+                          groupValue: _groupRadio,
+                          onChanged: (value) {
+                            setState(() {
+                              _groupRadio = value;
+                            });
+                          },
+                          activeColor: Color(0xFF6200EE),
+                        ),
+                        Divider(
+                          color: Colors.grey,
+                          indent: 5,
+                          endIndent: 5,
+                          thickness: 2,
+                          height: 10,
+                        )
+                      ],
                     ),
                 ],
               ),
@@ -98,33 +125,50 @@ class _FirstPageState extends State<FirstPage> {
             Container(
               // width: 100,
               alignment: Alignment.center,
-              child: Slider(
-                value: _slider,
-                min: 0,
-                max: 100,
-                label: _slider.round().toString(),
-                divisions: 10,
-                onChanged: (value) {
-                  if (value == 50) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        action: SnackBarAction(
-                          label: 'hide',
-                          onPressed: () {
-                            ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                          },
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Slider(
+                      value: _slider,
+                      min: 0,
+                      max: 100,
+                      label: _slider.round().toString(),
+                      divisions: 10,
+                      onChanged: (value) {
+                        if (value == 50) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              action: SnackBarAction(
+                                label: 'hide',
+                                onPressed: () {
+                                  ScaffoldMessenger.of(context)
+                                      .hideCurrentSnackBar();
+                                },
+                              ),
+                              content: Text(
+                                'half slider',
+                              ),
+                              duration: Duration(seconds: 3),
+                            ),
+                          );
+                        }
+                        setState(() {
+                          _slider = value;
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                      flex: 5,
+                      child: ListView.builder(
+                        itemCount: _slider.toInt(),
+                        itemBuilder: (context, index) => ListTile(
+                          leading: Icon(Icons.home),
+                          title: Text('home'),
                         ),
-                        content: Text(
-                          'half slider',
-                        ),
-                        duration: Duration(seconds: 3),
-                      ),
-                    );
-                  }
-                  setState(() {
-                    _slider = value;
-                  });
-                },
+                      ))
+                ],
               ),
             ),
           ],
